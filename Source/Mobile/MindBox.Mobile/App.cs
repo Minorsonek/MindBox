a469718524e4e71e732d81264d5713d1bbb4f3d8
Minorsonek <minorsonek@gmail.com>
2019-02-11 16:32:59 +0100

    Added mvvm pattern to the project

---
+++ Source/Mobile/MindBox.Mobile/App.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.ViewModels;
+
+namespace Mindbox.Mobile
+{
+    /// <summary>
+    /// The main entry point for mobile application, required class for MVVMCross
+    /// </summary>
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
---
+++ Source/Mobile/MindBox.Mobile/ViewModels/PlaygroundViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MvvmCross.ViewModels;
+
+namespace Mindbox.Mobile
+{
+	/// <summary>
+	/// The view model for playground page
+	/// </summary>
+    public class PlaygroundViewModel : MvxViewModel
+    {
+        #region Public Properties
+
+
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public PlaygroundViewModel()
+		{
+			
+		}
+
+        #endregion
+    }
+}
